661596dcd86c7060c31d0e920224d57b00d12e05
Chenzo88 <109112438+Chenzo88@users.noreply.github.com>
2022-10-12 06:49:07 -0300

    Aca va todo!!

---
+++ appP5eLearning/force-app/main/default/lwc/unitContent/unitContent.js
@@ -0,0 +1,32 @@
+import { LightningElement, wire, api } from 'lwc';
+import getUnitWrapper from '@salesforce/apex/UnitService.getUnitWrapper';
+
+export default class UnitContent extends LightningElement {
+
+@api recordId
+unit;
+questionList;
+
+
+@wire(getUnitWrapper, { unitId:'$recordId'})//'$recordId'
+    wiredData(result) {
+       const {data,error} = result;    
+      if (data) { 
+        console.log('ESTO ES TODO UNIT CONTENT----------',JSON.stringify(data));
+        //this.objectData= JSON.stringify({data});
+        //this.mapOfValues= JSON.parse(this.objectData);
+        this.unit= data.unit;   
+        this.questionList = data.questionList;
+        console.log('ESTO ES QUESTION LIST----------',this.questionList);
+        console.log('ESTO ES unit----------',this.unit);
+     
+              } else if (error) {
+         console.error('Error-->', error);
+      }
+    }
+
+
+
+
+
+}
